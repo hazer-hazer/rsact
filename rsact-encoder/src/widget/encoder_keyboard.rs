5fbf22bec76b2407f2414568e9519cc00654a2d3
hazer-hazer <gordeev.john@gmail.com>
2024-12-02 14:38:50 +0300

    Implement EncoderKeyboard as custom widget

--- rsact-encoder/src/widget/encoder_keyboard.rs
+++ rsact-encoder/src/widget/encoder_keyboard.rs
@@ -1,25 +1,51 @@
 use core::marker::PhantomData;
-use rsact_ui::prelude::*;
+use layout::{flex::flex_content_size, size::RectangleExt};
+use rsact_icons::system::SystemIcon;
+use rsact_ui::{
+    embedded_graphics::prelude::{Point, Transform},
+    prelude::*,
+    render::Renderable,
+};
+
+// pub trait EncoderKeyboardEvent {
+//     fn as_encoder_keyboard_select(&self) -> Option<i32>;
+// }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct SelectKeyboardState {
+pub struct EncoderKeyboardState {
     pressed: bool,
     active: bool,
-    // TODO: Merge focused with active into Option<usize>
-    focused: usize,
+    // TODO: Merge with active?
+    selected: usize,
 }
 
-impl SelectKeyboardState {
+impl EncoderKeyboardState {
     fn none() -> Self {
-        Self { pressed: false, active: false, focused: 0 }
+        Self { pressed: false, active: false, selected: 0 }
     }
 }
 
-// declare_widget_style! {
-//     SelectKeyboardStyle (SelectKeyboardState) {
-//         container: container,
-//     }
-// }
+declare_widget_style! {
+    EncoderKeyboardStyle (EncoderKeyboardState) {
+        container: container,
+        selected: container {
+            selected_background_color: background_color,
+            selected_border_color: border_color,
+            selected_border_radius: border_radius,
+        },
+    }
+}
+
+impl<C: Color> EncoderKeyboardStyle<C> {
+    pub fn base() -> Self {
+        Self {
+            container: BlockStyle::base(),
+            selected: BlockStyle::base().border(
+                BorderStyle::base().radius(5).color(C::default_foreground()),
+            ),
+        }
+    }
+}
 
 // pub enum SelectKeyboardBtn {
 //     Push(char),
@@ -36,23 +62,60 @@ impl SelectKeyboardState {
 //     fn buttons() -> impl Iterator<Item = &'static str>;
 // }
 
+pub enum EncoderKeyboardKind {
+    Char(char),
+    Delete,
+    Done,
+    // TODO: CapsLock
+}
+
+pub struct EncoderKeyboardKey<W: WidgetCtx> {
+    el: El<W>,
+    kind: EncoderKeyboardKind,
+}
+
+impl<W: WidgetCtx> EncoderKeyboardKey<W>
+where
+    W::Styler: WidgetStylist<MonoTextStyle<W::Color>>
+        + WidgetStylist<IconStyle<W::Color>>,
+{
+    fn new(inner: El<W>, kind: EncoderKeyboardKind) -> Self {
+        Self { el: Container::new(inner).padding(5u32).el(), kind }
+    }
+
+    fn char(char: char) -> Self {
+        Self::new(MonoText::new(char).el(), EncoderKeyboardKind::Char(char))
+    }
+
+    fn delete() -> Self {
+        Self::new(
+            Icon::new(SystemIcon::Cancel).el(),
+            EncoderKeyboardKind::Delete,
+        )
+    }
+
+    fn done() -> Self {
+        Self::new(Icon::new(SystemIcon::Check).el(), EncoderKeyboardKind::Done)
+    }
+}
+
 /**
  * Keyboard for use with encoder to input text.
  */
 pub struct EncoderKeyboard<W: WidgetCtx, Dir: Direction> {
     id: ElId,
-    buttons_count: usize,
-    content: Scrollable<W, Dir>,
-    state: Signal<SelectKeyboardState>,
+    layout: Signal<Layout>,
+    keys: Vec<EncoderKeyboardKey<W>>,
+    value: Signal<String>,
+    state: Signal<EncoderKeyboardState>,
+    style: MemoChain<EncoderKeyboardStyle<W::Color>>,
     dir: PhantomData<Dir>,
 }
 
 impl<W: WidgetCtx> EncoderKeyboard<W, RowDir>
 where
-    W::Styler: WidgetStylist<ButtonStyle<W::Color>>
-        + WidgetStylist<MonoTextStyle<W::Color>>
-        + WidgetStylist<ScrollableStyle<W::Color>>,
-    W::Event: ButtonEvent + ScrollEvent,
+    W::Styler: WidgetStylist<MonoTextStyle<W::Color>>
+        + WidgetStylist<IconStyle<W::Color>>,
 {
     pub fn row(value: Signal<String>) -> Self {
         Self::new(value)
@@ -61,10 +124,8 @@ where
 
 impl<W: WidgetCtx> EncoderKeyboard<W, ColDir>
 where
-    W::Styler: WidgetStylist<ButtonStyle<W::Color>>
-        + WidgetStylist<MonoTextStyle<W::Color>>
-        + WidgetStylist<ScrollableStyle<W::Color>>,
-    W::Event: ButtonEvent + ScrollEvent,
+    W::Styler: WidgetStylist<MonoTextStyle<W::Color>>
+        + WidgetStylist<IconStyle<W::Color>>,
 {
     pub fn col(value: Signal<String>) -> Self {
         Self::new(value)
@@ -73,155 +134,183 @@ where
 
 impl<W: WidgetCtx, Dir: Direction + 'static> EncoderKeyboard<W, Dir>
 where
-    W::Styler: WidgetStylist<ButtonStyle<W::Color>>
-        + WidgetStylist<MonoTextStyle<W::Color>>
-        + WidgetStylist<ScrollableStyle<W::Color>>,
-    W::Event: ButtonEvent + ScrollEvent,
+    W::Styler: WidgetStylist<MonoTextStyle<W::Color>>
+        + WidgetStylist<IconStyle<W::Color>>,
 {
-    pub fn new(mut value: Signal<String>) -> Self {
-        let mut state = SelectKeyboardState::none().signal();
-        let mut buttons = ('a'..='z')
-            .map(|char| {
-                Button::new(char)
-                    .on_click(move || {
-                        println!("Add char {char}");
-                        value.update(|value| value.push(char));
-                    })
-                    .el()
-            })
+    pub fn new(value: Signal<String>) -> Self {
+        let state = EncoderKeyboardState::none().signal();
+        let style = EncoderKeyboardStyle::base().memo_chain();
+
+        let mut keys = ('a'..='z')
+            .map(|char| EncoderKeyboardKey::char(char))
             .collect::<Vec<_>>();
 
-        buttons.push(
-            Button::new("DEL")
-                .on_click(move || {
-                    value.update(|value| value.pop());
-                })
-                .el(),
-        );
-        buttons.push(
-            Button::new("ENTER")
-                .on_click(move || {
-                    state.update(|state| state.active = false);
-                })
-                .el(),
-        );
+        keys.push(EncoderKeyboardKey::delete());
+        keys.push(EncoderKeyboardKey::done());
 
-        let buttons_count = buttons.len();
-
-        // TODO: API for flex gaps, padding, etc.
-        let content = Flex::<_, Dir>::new(buttons).gap(5);
-        let content = Scrollable::<_, Dir>::new(content).tracker();
+        let layout = Layout::shrink(LayoutKind::Flex(
+            FlexLayout::base(
+                Dir::AXIS,
+                flex_content_size(Dir::AXIS, keys.iter().map(|key| &key.el))
+                    .inert()
+                    .memo(),
+            )
+            .block_model(BlockModel::zero().padding(2)),
+        ))
+        .signal();
 
         Self {
             id: ElId::unique(),
-            buttons_count,
-            content,
+            layout,
+            keys,
+            value,
             state,
+            style,
             dir: PhantomData,
         }
     }
 }
 
-impl<W: WidgetCtx, Dir: Direction> SizedWidget<W> for EncoderKeyboard<W, Dir>
+impl<W: WidgetCtx, Dir: Direction> SizedWidget<W> for EncoderKeyboard<W, Dir> where
+    W::Styler: WidgetStylist<EncoderKeyboardStyle<W::Color>>
+{
+}
+
+impl<W: WidgetCtx, Dir: Direction> BlockModelWidget<W>
+    for EncoderKeyboard<W, Dir>
 where
-    W::Styler: WidgetStylist<ButtonStyle<W::Color>>
-        + WidgetStylist<MonoTextStyle<W::Color>>
-        + WidgetStylist<ScrollableStyle<W::Color>>,
-    W::Event: ButtonEvent + ScrollEvent,
+    W::Styler: WidgetStylist<EncoderKeyboardStyle<W::Color>>,
 {
 }
 
 impl<W: WidgetCtx, Dir: Direction> Widget<W> for EncoderKeyboard<W, Dir>
 where
-    W::Styler: WidgetStylist<ButtonStyle<W::Color>>
-        + WidgetStylist<MonoTextStyle<W::Color>>
-        + WidgetStylist<ScrollableStyle<W::Color>>,
-    W::Event: ButtonEvent + ScrollEvent,
-    // where
-    //     W::Styler: WidgetStylist<SelectKeyboardStyle<W::Color>>,
+    W::Styler: WidgetStylist<EncoderKeyboardStyle<W::Color>>,
 {
     fn meta(&self) -> rsact_ui::widget::MetaTree {
-        let id = self.id;
-        let content_meta = self.content.meta();
-
-        MetaTree {
-            data: self.state.map(move |state| {
-                if state.active {
-                    Meta::none()
-                } else {
-                    Meta::focusable(id)
-                }
-            }),
-            children: self.state.map(move |state| {
-                if state.active {
-                    vec![content_meta]
-                } else {
-                    vec![]
-                }
-            }),
-        }
+        MetaTree::childless(Meta::focusable(self.id).inert())
     }
 
     fn on_mount(&mut self, ctx: rsact_ui::widget::MountCtx<W>) {
-        // ctx.accept_styles(self.style, self.state);
-        self.content.on_mount(ctx);
+        // TODO
+        ctx.accept_styles(self.style, self.state);
     }
 
     fn layout(&self) -> Signal<Layout> {
-        self.content.layout()
+        self.layout
     }
 
     fn build_layout_tree(&self) -> MemoTree<Layout> {
-        self.content.build_layout_tree()
+        MemoTree {
+            data: self.layout.memo(),
+            children: self
+                .keys
+                .iter()
+                .map(|key| key.el.build_layout_tree())
+                .collect::<Vec<_>>()
+                .inert()
+                .memo(),
+        }
     }
 
     fn draw(&self, ctx: &mut DrawCtx<'_, W>) -> DrawResult {
-        ctx.draw_focus_outline(self.id)?;
+        let state = self.state.get();
+        let style = self.style.get();
 
-        self.content.draw(ctx)
+        if !state.active {
+            ctx.draw_focus_outline(self.id)?;
+        }
+
+        let keys_offset = {
+            let selected_key_layout =
+                ctx.layout.children().nth(state.selected).unwrap();
+
+            let selected_key_offset =
+                ctx.layout.inner.center_offset_of(selected_key_layout.inner);
+
+            if state.active {
+                Block::from_layout_style(
+                    selected_key_layout.outer.translate(selected_key_offset),
+                    BlockModel::zero().border_width(1),
+                    style.selected,
+                )
+                .render(ctx.renderer)?;
+            }
+
+            selected_key_offset
+        };
+
+        ctx.renderer.clipped(ctx.layout.inner, |renderer| {
+            self.keys.iter().zip(ctx.layout.children()).try_for_each(
+                |(key, key_layout)| {
+                    let mut ctx = DrawCtx {
+                        state: ctx.state,
+                        renderer,
+                        layout: &key_layout.translate(keys_offset),
+                        tree_style: ctx.tree_style,
+                    };
+
+                    key.el.draw(&mut ctx)
+                },
+            )
+        })
     }
 
     fn on_event(&mut self, ctx: &mut EventCtx<'_, W>) -> EventResponse<W> {
         let state = self.state.get();
 
-        if state.active {
+        if state.active && ctx.is_focused(self.id) {
             if let Some(offset) = ctx.event.as_focus_move() {
-                let prev_focused = state.focused;
-                let new_focused = self.state.update(|state| {
-                    state.focused = (state.focused as i64 + offset as i64)
-                        .clamp(0, self.buttons_count as i64)
-                        as usize;
-                    state.focused
-                });
+                let current = state.selected;
 
-                // Intercept focusing event and define local focusing logic.
-                ctx.bubble(BubbledData::FocusOffset(
-                    new_focused as i32 - prev_focused as i32,
-                ))
-            } else {
-                // Deactivation done on "Enter" button press
-                ctx.pass_to_child(&mut self.content)
-            }
-        } else {
-            ctx.handle_focusable(self.id, |ctx, press| {
-                if state.pressed != press {
-                    // Always activates,
-                    let activate = !state.pressed && press;
+                let new = (current as i32 + offset)
+                    .clamp(0, self.keys.len().saturating_sub(1) as i32)
+                    as usize;
 
+                if new != current {
                     self.state.update(|state| {
-                        state.pressed = press;
-                        if activate {
-                            state.active = true;
-                        }
+                        state.selected = new;
+                    });
+                }
+
+                return ctx.capture();
+            }
+        }
+
+        ctx.handle_focusable(self.id, |ctx, pressed| {
+            if state.pressed != pressed {
+                if state.active {
+                    let key = &self.keys[state.selected];
+
+                    match key.kind {
+                        EncoderKeyboardKind::Char(char) => {
+                            self.value.update(|value| value.push(char))
+                        },
+                        EncoderKeyboardKind::Delete => {
+                            self.value.update(|value| {
+                                value.pop();
+                            })
+                        },
+                        EncoderKeyboardKind::Done => {
+                            self.state.update(|state| state.active = false);
+                        },
+                    }
+
+                    ctx.capture()
+                } else if !state.pressed && pressed {
+                    // Enter edit mode
+                    self.state.update(|state| {
+                        state.active = true;
                     });
 
-                    // Refocusing since focus is changed. The target element in the tree is the first button in the keyboard.
-                    ctx.bubble(BubbledData::FocusOffset(0))
+                    ctx.capture()
                 } else {
                     ctx.ignore()
                 }
-            })
-        }
+            } else {
+                ctx.ignore()
+            }
+        })
     }
 }
 
