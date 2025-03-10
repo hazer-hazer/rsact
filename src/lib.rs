c3659330dce7c3276b7ecddb22b5a371603885b1
hazer-hazer <gordeev.john@gmail.com>
2025-03-10 21:03:46 +0300

    Preparing for real-life usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod prelude {
     // pub use rsact_reactive::prelude::*;
     pub use rsact_ui::prelude::*;
 }
 
-pub use rsact_encoder as encoder;
+// pub use rsact_encoder as encoder;
 // pub use rsact_reactive as reactive;
 pub use rsact_ui as ui;
